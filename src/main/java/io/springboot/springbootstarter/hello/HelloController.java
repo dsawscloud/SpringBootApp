6100706b303b46f41532add47f358ce8e6572e34
dsawscloud <dsawscloud@gmail.com>
2021-08-20 00:16:44 -0500

    commit

--- src/main/java/io/springboot/springbootstarter/hello/HelloController.java
+++ src/main/java/io/springboot/springbootstarter/hello/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String sayHi() {
-		return "Hi Shiva!";
+		return "Hi Shiva !";
 	}
 
 }
